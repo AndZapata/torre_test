c6ebd711f00a545dc4a6f6d6b6d7b3dd58a6bbe5
AndZapata <andzapata514@gmail.com>
2019-11-11 17:02:41 +0000

    [Fix] button style and dropdown style

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -6,7 +6,7 @@ export class Home extends Component {
       <div className="container text-center mt-4">
         <h1>Looking for Top 10 people</h1>
         <div className="d-flex justify-content-center mt-4">
-          <div className="input-group input-group-lg col-4">
+          <div className="input-group input-group-lg col-5">
             <div className="input-group-prepend">
               <span className="input-group-text" style={{ backgroundColor: "#20B6C9", color: "#ffffff" }} id="inputGroup-sizing-default">Name</span>
             </div>
@@ -16,8 +16,8 @@ export class Home extends Component {
             <button type="button" className="btn btn-light dropdown-toggle" style={{ backgroundColor: "#20B6C9", color: "#ffffff" }} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.props.onDisplayCompanies}>
               Companies
  </button>
-            <div className="dropdown-menu col-11">
-              <ul className="list-group" style={{overflowY: "auto"}}>
+            <div className="dropdown-menu col-11" style={{height: "40vh", overflowY: "scroll"}}>
+              <ul className="list-group">
                 {this.props.techCompany.map(item => (
                   <li key={item._id} className="list-group-item d-flex justify-content-between align-items-center">
                   {item.techCompany}
@@ -26,8 +26,8 @@ export class Home extends Component {
                 ))}
               </ul>
             </div>
-            <div className="btn" onClick={this.props.searchPeople}>Search</div>
           </div>
+          <div className="btn btn-light col-3" style={{ backgroundColor: "#20B6C9", color: "#ffffff" }} onClick={this.props.searchPeople}>Search</div>
         </div>
       </div>
     );
