160b0e94eb9923925e5062813a405f2043937b3d
AndZapata <andzapata514@gmail.com>
2019-11-12 04:26:53 +0000

    [Feature] generate CRUD management for data route at API

---
+++ backend/routes/data.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const Data = require('../models/Data');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  // get all tech companies from database
+  try {
+    const data = await Data.find();
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.get('/:dataId', async (req, res) => {
+  // Get company by stack id
+  try {
+    const data = await Data.findById(req.params.dataId);
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new stack for ranking comparison
+  try {
+    const data = new Data(req.body);
+    const savedData = await data.save();
+    res.json(savedData);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.delete('/:dataId', async (req, res) => {
+  // Delete company by stack id
+  try {
+    const removedData = await Data.deleteOne({
+      _id: req.params.dataId
+    });
+    res.json(removedData);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.patch('/:dataId', async (req, res) => {
+  // Update company by stackId
+  try {
+    const updatedData = await Data.findByIdAndUpdate(
+      { _id: req.params.dataId },
+      {
+        $push: {
+          username: req.body.username,
+          name: req.body.name,
+          strengths: req.body.strengths
+        }
+      }
+    );
+    res.json(updatedData);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
